808ea05e6a50ada489001fa4e6688fbd3a870a97
ngotienphat <quangphatc3@gmail.com>
2016-07-21 14:32:16 +0700

    ppp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function (){
+$(document).ready(function () {
     var myarr = {
         component: '',
         column: ''
-    }
-    
-    
+    };
+
+
     $('.page_right .column_select').draggable({
-        containment: 'document',helper: 'clone',
-        start: function (){
+        containment: 'document', helper: 'clone',
+        start: function () {
             components = $(this).attr('data-components1');
-            switch(components) {
+            switch (components) {
                 case 'Column1':
                     myarr.column = '<div class="row"><div class="col-md-12 column_sel"></div></div>';
                     break;
@@ -29,10 +29,10 @@ $(document).ready(function (){
         }
     });
     $('.page_right .component').draggable({
-        containment: 'document',helper: 'clone',
-        start: function (){
+        containment: 'document', helper: 'clone',
+        start: function () {
             components = $(this).attr('data-components');
-            switch(components) {
+            switch (components) {
                 case 'Text':
                     myarr.component = '<div class="form-group">';
                     myarr.component += '    <label class="col-md-4 control-label" for="textinput">Text Input</label>  ';
@@ -78,7 +78,7 @@ $(document).ready(function (){
                 case 'Header':
                     myarr.component = '<div class="form-group">';
                     myarr.component += '    <div class="col-md-12">';
-                    myarr.component += '         <label>This is a label</label>';
+                    myarr.component += '         <h4><label>This is a label</label></h4>';
                     myarr.component += '    </div>';
                     myarr.component += '</div>';
                     break;
@@ -95,16 +95,22 @@ $(document).ready(function (){
     });
     $('.page_left').droppable({
         accept: '.column_select',
-        drop: function (){
+        drop: function () {
             $('.dropable').append(myarr.column);
-            $('.column_sel').droppable({
-                accept: '.component',
-                drop: function (){
-                    $(this).append(myarr.component);
-                }
-            });
+            set_drop();
         }
     });
     
+    set_drop();
+    
+    function set_drop() {
+        $('.column_sel').droppable({
+            accept: '.component',
+            drop: function () {
+                $(this).append(myarr.component);
+            }
+        });
+    }
+
 });
 
