3a210e0bb1876dbbfd9206b4c03f558a70f26196
ngotienphat <quangphatc3@gmail.com>
2016-07-21 13:47:46 +0700

    ppp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,162 +1,68 @@
-$(function () {
-    var myarr = {};
-    myarr = {
-        name: '',
-        data: '',
-        column: ''
-    };
-    function check_column(num) {
-        switch (num) {
-            case '1':
-                myarr.column = '<div class="row r_box_component">';
-                myarr.column += '   <div class="col-md-12">';
-                myarr.column += '      <div class="box_com">';
-                myarr.column += '      </div>';
-                myarr.column += '  </div>';
-                myarr.column += '</div>';
-                break;
-            case '2':
-                myarr.column = '<div class="row r_box_component">';
-                myarr.column += '<div class="col-md-6">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '</div>';
-                myarr.column += '<div class="col-md-6">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '  </div>';
-                myarr.column += '</div>';
-                break;
-            case '3':
-                myarr.column = '<div class="row r_box_component">';
-                myarr.column += '<div class="col-md-4">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '</div>';
-                myarr.column += '<div class="col-md-4">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '</div>';
-                myarr.column += '<div class="col-md-4">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '  </div>';
-                myarr.column += '</div>';
-                break;
-            case '4':
-                myarr.column = '<div class="row r_box_component">';
-                myarr.column += '<div class="col-md-3">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '</div>';
-                myarr.column += '<div class="col-md-3">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '</div>';
-                myarr.column += '<div class="col-md-3">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '</div>';
-                myarr.column += '<div class="col-md-3">';
-                myarr.column += '    <div class="box_com">';
-                myarr.column += '    </div>';
-                myarr.column += '  </div>';
-                myarr.column += '</div>';
-                break;
-        }
-    }
-    function check_components(type) {
-        switch (type) {
-            case 'Text_Field':
-                myarr.data = '<div class="form-group box_component">';
-                myarr.data += '   <label class="col-md-4 control-label" for="passwordinput">Label</label>';
-                myarr.data += '   <div class="col-md-8">';
-                myarr.data += '      <input id="passwordinput" name="passwordinput" type="text" placeholder="placeholder" class="form-control input-md">';
-                myarr.data += '   </div>';
-                myarr.data += '</div>';
-                break;
-            case 'Text_Area':
-                myarr.data = '<div class="form-group box_component">';
-                myarr.data += '   <label class="col-md-4 control-label" for="passwordinput">Label</label>';
-                myarr.data += '   <div class="col-md-8">';
-                myarr.data += '      <textarea name="" class="form-control" cols="10" rows="2">This is Textarea</textarea>';
-                myarr.data += '   </div>';
-                myarr.data += '</div>';
-                break;
-            case 'Radio':
-                myarr.data = '<div class="form-group box_component">';
-                myarr.data += '   <label class="col-md-4 control-label" for="passwordinput">Label</label>';
-                myarr.data += '   <div class="col-md-8">';
-                myarr.data += '      <input type="radio"  name="radio" id="" value="1"/> 1';
-                myarr.data += '      <input type="radio"  name="radio" id="" value="1"/> 2';
-                myarr.data += '   </div>';
-                myarr.data += '</div>';
-                break;
-            case 'Checkbox':
-                myarr.data = '<div class="form-group box_component">';
-                myarr.data += '   <label class="col-md-4 control-label" for="passwordinput">Label</label>';
-                myarr.data += '   <div class="col-md-8">';
-                myarr.data += '      <input type="checkbox" name="" id="" value="1"/> 1';
-                myarr.data += '      <input type="checkbox" name="" id="" value="1"/> 2';
-                myarr.data += '   </div>';
-                myarr.data += '</div>';
-                break;
-            case 'Select':
-                myarr.data = '<div class="form-group box_component">';
-                myarr.data += '   <label class="col-md-4 control-label" for="passwordinput">Label</label>';
-                myarr.data += '   <div class="col-md-8">';
-                myarr.data += '      <select name="" class="form-control"><option value="1">Option 1</option><option value="2">Option 2</option></select>';
-                myarr.data += '   </div>';
-                myarr.data += '</div>';
-                break;
-            case 'Header':
-                myarr.data = '<div class="form-group box_component">';
-                myarr.data += '   <label class="col-md-12 control-label">Header</label>';
-                myarr.data += '</div>';
-                break;
-            case 'Date_Field':
-                myarr.data = '<div class="form-group box_component">';
-                myarr.data += '   <label class="col-md-4 control-label" for="passwordinput">Label</label>';
-                myarr.data += '   <div class="col-md-8">';
-                myarr.data += '      <input type="date" class="form-control" name="" id=""/>';
-                myarr.data += '   </div>';
-                myarr.data += '</div>';
-                break;
-        }
-    }
-
-    set_drag('column');
-    set_drag('components');
-    
-    function set_drag(component) {
-        $('.item_dragable').draggable({
-            helper: "clone", revert: "invalid",
-            start: function () {
-                var selector = $(this).attr('data-' + component);
-                check_column(selector);
-                console.log(selector);
+$(document).ready(function (){
+    $('.page_right .column_select').draggable({
+        containment: 'document',helper: 'clone',
+        start: function (){
+            components = $(this).attr('data-components1');
+            switch(components) {
+                case 'Column1':
+                    contentsa = '<div class="col-md-12 page_left1"></div>';
+                    break;
+                case 'Column2':
+                    contentsa = '<div class="row"><div class="col-md-6 page_left1"></div><div class="col-md-6 page_left1"></div></div>';
+                    break;
+                case 'Column3':
+                    contentsa = '<div class="row"><div class="col-md-4 page_left1"></div><div class="col-md-4 page_left1"></div><div class="col-md-4 page_left1"></div></div>';
+                    break;
+                case 'Column4':
+                    contentsa = '<div class="row"><div class="col-md-3 page_left1"></div><div class="col-md-3 page_left1"></div><div class="col-md-3 page_left1"></div><div class="col-md-3 page_left1"></div></div>';
+                    break;
+                default:
+                    contentsa = '<div class="col-md-12 page_left1"></div>';
+                    break;
             }
-        });
-    }
-    //Get value input radio
-    $('.droppable').droppable({
-        drop: function () {
-            $('.droppable .droped').append(myarr.column);
-            $('.box_com').each(function () {
-                $(this).droppable({
-                    drop: function () {
-                        $(this).append(myarr.data);
-                    }
-                });
+        }
+    });
+    $('.page_right .component').draggable({
+        containment: 'document',helper: 'clone',
+        start: function (){
+            components = $(this).attr('data-components');
+            switch(components) {
+                case 'Text':
+                    contents = '<span class="chinh1"><lable>Text:</lable><input type="text" /></span><br>';
+                    break;
+                case 'Text Area':
+                    contents = '<span class="chinh1"><lable>Textarea:</lable><textarea></textarea></span><br>';
+                    break;
+                case 'Radio':
+                    contents = '<span class="chinh1">Radio: <input type="radio" /></span><br>';
+                    break;
+                     case 'Checkbox':
+                    contents = '<span class="chinh1">Checkbox:<input type="checkbox" /></span><br>';
+                    break;
+                case 'Select':
+                    contents = '<span class="chinh1">Select:<select></select></span><br>';
+                    break;
+                case 'Header':
+                    contents = '<span class="chinh1">Label:<label>header</label></span><br>';
+                    break;
+                default:
+                    contents = '<span class="chinh1"><lable>Text:</lable><input type="text" /></span><br>';
+                    break;
+            }
+        }
+    });
+    $('.page_left').droppable({
+        accept: '.column_select',
+        drop: function (){
+            $('.page_left').append(contentsa);
+            $('.page_left1').droppable({
+                accept: '.component',
+                drop: function (){
+                    $(this).append(contents);
+                }
             });
         }
     });
-    $('.box_com').each(function () {
-        $(this).droppable({
-            drop: function () {
-                $(this).append(myarr.data);
-            }
-        });
-    });
-
+    
 });
+
