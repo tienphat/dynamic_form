b9c7736f35eb8ad52133c3236c339404b8f0530b
ngotienphat <quangphatc3@gmail.com>
2016-07-21 17:24:13 +0700

    ppp

---
+++ assets/libs/locales/bootstrap-datepicker.vi.js
@@ -0,0 +1,15 @@
+/**
+ * Bulgarian translation for bootstrap-datepicker
+ * Apostol Apostolov <apostol.s.apostolov@gmail.com>
+ */
+;
+(function($) {
+    $.fn.datepicker.dates['vi'] = {
+        days: ['Chủ Nhật', 'Thứ Hai', 'Thứ Ba', 'Thứ Tư', 'Thứ Năm', 'Thứ Sáu', 'Thứ Bảy'],
+        daysShort: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
+        daysMin: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
+        months: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"],
+        monthsShort: ["Thg1", "Thg2", "Thg3", "Thg4", "Thg5", "Thg6", "Thg7", "Thg8", "Thg9", "Thg10", "Thg11", "Thg12"],
+        today: "Hôm nay"
+    };
+}(jQuery));
